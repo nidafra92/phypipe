c4477dd5549d2548ab7e67613a1884f69817b190
Nicolás David Franco Sierra <nidafra92@gmail.com>
2017-10-27 19:52:07 -0500

    Add main script

---
+++ phypipe2.py
@@ -0,0 +1,30 @@
+#/usr/bin/env python3
+
+"""
+PhyPipe2: Automated pipeline for phylogenetic reconstruction.
+
+Advantages over PhyPipe ver. 1.
+--------------------------------
+
+ * Many...
+ * ...
+ * :D
+
+Install
+---------
+
+  $ pip install ruffus --upgrade
+
+copy this script to somewhere in your PATH
+
+Usage
+--------
+
+  $ phypipe2 [options] <file/folder>
+
+use option '- '
+
+"""
+
+import sys
+import ruffus
